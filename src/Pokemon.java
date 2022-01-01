df174c68d9db688160829b8a35722209f827be03
Giovanni <giovanni.f.liguori@gmail.com>
2022-01-01 20:29:14 +0100

    pokemon id: 0 -> 261

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -7,7 +7,7 @@ import java.net.URL;
 public class Pokemon
 {
 	private final URL urlGetPokemon = new URL("https://pokeapi.co/api/v2/pokemon/"); //  dopo pokemon/ inserire numero_id oppure nome
-	private static int pokemon_id = 0;
+	private static int pokemon_id = 261;
 	// https://pokeapi.co/api/v2/pokemon/261/ -> Poochyena
 	
 	private String nome;
