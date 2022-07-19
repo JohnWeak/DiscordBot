7efb3a7929d320667dffff9b3679067f7cb250de
Giovanni <giovanni.f.liguori@gmail.com>
2022-07-19 12:38:30 +0200

    Fix generali

--- src/Commands.java
+++ src/Commands.java
@@ -536,8 +536,8 @@ public class Commands extends ListenerAdapter
 		
 		if (x.length() > 2000)
 			 split = x.substring(0, 1999);
-		
-		System.out.println("\n\n"+split+"\n\n");
+		else
+			channel.sendMessage(x).queue();
 		
 		try
 		{
