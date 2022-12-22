b7d74ffd9ae881cdfc2bdfb678d9037e9623616f
Giovanni <giovanni.f.liguori@gmail.com>
2022-12-22 22:04:41 +0100

    Fix generali

--- src/Commands.java
+++ src/Commands.java
@@ -279,7 +279,7 @@ public class Commands extends ListenerAdapter
 					catch (Exception exception)
 					{
 						// siccome bot on server, adesso mi deve taggare per le eccezioni
-						var spamErrore = "<@180759114291478528>\n" + exception;
+						var spamErrore = "<@180759114291478528>\n" + exception.getMessage();
 						canaleBotPokemon.sendMessage(spamErrore).queue();
 					}
 				}
