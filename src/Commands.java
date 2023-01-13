a7b2c1050823b24377a68edd689508e0d55a6026
Giovanni <giovanni.f.liguori@gmail.com>
2023-01-13 15:37:11 +0100

    Fix generali

--- src/Commands.java
+++ src/Commands.java
@@ -245,7 +245,6 @@ public class Commands extends ListenerAdapter
 	
 	/** Controlla che il messaggio abbia le parole chiave per attivare i comandi (o le reazioni) del bot
 	 * @param msgLowerCase la stringa del messaggio convertita in minuscolo
-	 * @return void
 	 * */
 	public void checkForKeywords(String msgLowerCase)
 	{
