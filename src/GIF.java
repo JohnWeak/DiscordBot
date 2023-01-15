fc40340cbf6cc5ed3e77995065ce9ae6bffce373
Giovanni <giovanni.f.liguori@gmail.com>
2023-01-15 13:49:57 +0100

    Fix generali

--- src/GIF.java
+++ src/GIF.java
@@ -1,8 +1,9 @@
-/**Questa classe contiene gli indirizzi delle Bot.GIF che usa il bot.*/
+/**Questa classe contiene gli indirizzi URL delle GIF.*/
 public abstract class GIF
 {
 	public final static String spyHang = "https://c.tenor.com/pLhXMqtRw2gAAAAC/team-fortress-spy.gif";
 	public final static String eggdog = "https://thumbs.gfycat.com/GregariousDevotedArachnid-mobile.mp4";
 	public final static String engineer = "https://i.warosu.org/data/diy/img/0014/73/1538282856261.gif";
+	// public final static String next_one = "";
 
-} // fine classe Bot.GIF
+} // fine classe GIF
