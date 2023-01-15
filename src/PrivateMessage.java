9024538ab6322d3f72a21faeff55ee5fe904c3ac
Giovanni <giovanni.f.liguori@gmail.com>
2023-01-15 13:43:56 +0100

    Fix generali

--- src/PrivateMessage.java
+++ src/PrivateMessage.java
@@ -4,11 +4,10 @@ import net.dv8tion.jda.api.entities.User;
 public class PrivateMessage
 {
 	private final User user;
-	private MessageChannel messageChannel;
+	private final MessageChannel messageChannel;
 	
 	/**Questa classe permette di inviare messaggi privati agli utenti passati tramite parametro
-	 * @param user Utente a cui inviare il messaggio privato.
-	 */
+	 * @param user Utente a cui inviare il messaggio privato. */
 	public PrivateMessage(User user)
 	{
 		this.user = user;
