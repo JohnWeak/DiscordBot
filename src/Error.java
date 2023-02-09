0a18689de2040e0f36052f6e5b67ae0bd7126511
Giovanni <giovanni.f.liguori@gmail.com>
2023-02-09 21:20:32 +0100

    Fix generali

--- src/Error.java
+++ src/Error.java
@@ -3,7 +3,7 @@ public abstract class Error
 	public static void print(Object object, Exception exception)
 	{
 	
-		String msg = "<@"+Utente.ID_GION+">\n"+"`"+object.getClass()+"`\n"+exception.getMessage()+"\n"+exception.getStackTrace()[0];
+		String msg = "<@"+Utente.ID_GION+">\n"+"`"+object+"`\n"+exception.getMessage()+"\n"+exception.getStackTrace()[0];
 		Commands.canaleBot.sendMessage(msg).queue();
 	
 	} // fine printError()
