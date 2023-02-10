621178ae9c0e33157ef570b103249cc54bdbd980
Giovanni <giovanni.f.liguori@gmail.com>
2023-02-10 11:53:28 +0100

    Fix generali

--- src/Error.java
+++ src/Error.java
@@ -7,7 +7,7 @@ public abstract class Error
 	
 		String msg = "<@"+Utente.ID_GION+">\n"+"`"+object+"`\n"+exception.getMessage()+"\n"+exception.getStackTrace()[0];
 		Commands.canaleBot.sendMessage(msg).queue();
-		new PrivateMessage(Utente.getGion()).send(Arrays.toString(exception.getStackTrace()));
+		new PrivateMessage(Utente.getGion()).send("`"+Arrays.toString(exception.getStackTrace())+"`");
 		
 	} // fine printError()
 	
