d8177e23d9b8fe672821930e320e484b67429369
Giovanni <giovanni.f.liguori@gmail.com>
2023-02-12 17:27:43 +0100

    Fix generali

--- src/Error.java
+++ src/Error.java
@@ -10,14 +10,15 @@ public class Error <T>
 		{
 			String lessThan2Thousand;
 			var e = (Exception) t;
+			var exMsg = e.getMessage();
 			var stackTrace = e.getStackTrace();
 			var stackTraceString = Arrays.toString(stackTrace);
 			
 			lessThan2Thousand = (stackTraceString.length() > 2000 ? stackTraceString.substring(0, 1999) : stackTraceString);
 			
-			String msg = "<@" + Utente.ID_GION + ">\n" + "`" + object + "`\n" + e.getMessage();
+			String msg = "<@" + Utente.ID_GION + ">\n" + "`" + object + "`\n" + exMsg;
 			//Commands.canaleBot.sendMessage(msg).queue();
-			gion.send("`"+e.getMessage()+"`\n"+lessThan2Thousand);
+			gion.send("`" + exMsg + "`\n"+lessThan2Thousand);
 		}
 		else if (type.equalsIgnoreCase("String"))
 		{
