cd7ae3377eb5f0abbabef8224c30d811048053a8
Giovanni <giovanni.f.liguori@gmail.com>
2024-12-23 22:53:39 +0100

    Fix generali

--- src/main/java/NewCommands.java
+++ src/main/java/NewCommands.java
@@ -10,8 +10,10 @@ public class NewCommands extends ListenerAdapter
 		final var a = event.getMessage().getContentDisplay();
 		final var b = event.getChannel().getName();
 		final var c = event.getGuildChannel().getName();
+		final var d = event.getAuthor().getName();
+		final var e = String.valueOf(event.getAuthor().isBot());
 		
-		final String reply = a.concat("\n").concat(b).concat("\n").concat(c).concat("\n").concat(String.valueOf(event.getAuthor().isBot()));
+		final String reply = a.concat("\n").concat(b).concat("\n").concat(c).concat("\n").concat(d).concat("\n").concat(e);
 		
 		System.out.println(reply);
 		
