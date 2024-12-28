5db01951d888174303569c6fcb1130ec42efa526
Giovanni <giovanni.f.liguori@gmail.com>
2024-12-28 22:09:08 +0100

    Fix generali

--- src/main/java/DailyTask.java
+++ src/main/java/DailyTask.java
@@ -1,34 +1,35 @@
-import net.dv8tion.jda.api.entities.channel.middleman.GuildMessageChannel;
 
 import java.time.LocalDate;
 import java.util.TimerTask;
 
 public class DailyTask extends TimerTask
 {
-	private final GuildMessageChannel canaleBot = Commands.canaleBot;
 	
 	@Override
 	public void run()
 	{
-		final boolean enigmosDaily = canaleBot.getHistory()
-			.retrievePast(15)
-			.complete()
-			.stream()
-			.anyMatch(message ->
-				message.getTimeCreated().toLocalDate().equals(LocalDate.now()) &&
-				message.getAuthor().getId().equals(Utente.ID_ENIGMO) &&
-				message.getContentRaw().strip().toLowerCase().contains("owo daily")
-			);
-		
-		if (!enigmosDaily)
+		try
 		{
-			final String tag = Utente.getEnigmo().getAsMention();
-			final String msg = "ENIGMO DOV'È IL TUO DAILY?!?!?!? PERCHÉ NON HAI FATTO ANCORA IL DAILY?!?!?!?!";
+			final boolean enigmosDaily = Commands.canaleBot.getHistory()
+				.retrievePast(15)
+				.complete()
+				.stream()
+				.anyMatch(message ->
+					message.getTimeCreated().toLocalDate().equals(LocalDate.now()) &&
+					message.getAuthor().getId().equals(Utente.ID_ENIGMO) &&
+					message.getContentRaw().strip().toLowerCase().contains("owo daily")
+				);
 			
-			try
+			if (!enigmosDaily)
 			{
-				canaleBot.sendMessage(tag + msg).queue();
-			}catch (Exception e) { new Error<Exception>().print(this, e); }
-		}
+				final String tag = Utente.getEnigmo().getAsMention();
+				final String msg = "ENIGMO DOV'È IL TUO DAILY?!?!?!? PERCHÉ NON HAI FATTO ANCORA IL DAILY?!?!?!?!";
+				
+				final String msgToSend = String.format("%s %s", tag, msg);
+				
+				Commands.canaleBot.sendMessage(msgToSend).queue();
+				
+			}
+		} catch (Exception e) { new Error<Exception>().print(this, e); }
 	}
 }
